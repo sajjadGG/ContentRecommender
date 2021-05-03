5744146c2a6aa9081c66369c79976550da2f4c57
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-05-03 22:39:54 +0430

    feature(db_connection):add connection to mongo token

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import pymongo
+from pymongo import MongoClient
+
+with open('.config' , 'r') as f:
+    l = f.readline()
+    mongo_token = l.split(':=')[-1]
+
+# print(mongo_token)
+client = MongoClient(mongo_token , serverSelectionTimeoutMS=360000)
+db = client['myFirstDatabase']
+print(db.list_collection_names())
