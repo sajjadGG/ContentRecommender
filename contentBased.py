d635725d3534bd768605a51dbd9963ceac213128
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-05-10 00:50:47 +0430

    feat('contentBased'):collect user histories and tranform to vector representation

--- contentBased.py
+++ contentBased.py
@@ -1,6 +1,8 @@
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
+from datetime import datetime , timedelta
+from DB import get_db
 #TODO : read from conf
 GLOVE_FILE = 'C:\\Dev\\RecommenderSystem\\w2v\\'
 GLOVE_TYPE = 'glove.6B.100d'
@@ -11,10 +13,55 @@ model = KeyedVectors.load_word2vec_format(word2vec_glove_file)
 print(model.most_similar('obama'))
 
 def collect_user_history():
-    pass
+    db = get_db()
+    #TODO : make query more efficient
+    c = db.userhistories.aggregate([
+        {
+            '$match' : {
+                'createdAt' : {
+                    '$gt' : datetime.now() - timedelta(days=3)
+                }
+            }
+        },
+        {
+            '$lookup' : {
+                'from' : 'users',
+                'localField' : 'user',
+                'foreignField' : '_id',
+                'as' : 'userdata'
+            }
+        },
+        {
+        "$unwind": "$userdata"
+        },
+        {
+            '$lookup' : {
+                'from' : 'contents',
+                'localField' : 'content',
+                'foreignField' : '_id',
+                'as' : 'contentdata'
+            }
+        },
+        {
+        "$unwind": "$contentdata"
+        },
+        {
+            '$group':{
+                '_id': {'user' : '$userdata.username'},
+                'contents' : {'$addToSet' : '$contentdata.des'}
+            }
+        }
+    ]) 
+    return c
 
-def user_history_to_vector():
-    pass
+def user_history_to_vector(c , model = model):
+    u = {}
+    for k in c:
+        user = k['_id']['user']
+        contents = l['contents']
+        #TODO : preprocess contents 
+        return sum([model.get_vector(w) for s in contents for w in s.split() if w in model])
+        
 
 def find_most_similar_contents():
     pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from datetime import datetime , timedelta
 with open('.config' , 'r') as f:
     l = f.readline()
     mongo_token = l.split(':=')[-1]
@@ -8,3 +8,41 @@ with open('.config' , 'r') as f:
 client = MongoClient(mongo_token , serverSelectionTimeoutMS=360000)
 db = client['myFirstDatabase']
 print(db.list_collection_names())
+
+c = db.userhistories.aggregate([
+        {
+            '$match' : {
+                'createdAt' : {
+                    '$gt' : datetime.now() - timedelta(days=3)
+                }
+            }
+        },
+        {
+            '$lookup' : {
+                'from' : 'users',
+                'localField' : 'user',
+                'foreignField' : '_id',
+                'as' : 'userdata'
+            }
+        },
+        {
+        "$unwind": "$userdata"
+        },
+        {
+            '$lookup' : {
+                'from' : 'contents',
+                'localField' : 'content',
+                'foreignField' : '_id',
+                'as' : 'contentdata'
+            }
+        },
+        {
+        "$unwind": "$contentdata"
+        },
+        {
+            '$group':{
+                '_id': {'user' : '$userdata.username'},
+                'contents' : {'$addToSet' : '$contentdata.des'}
+            }
+        }
+    ])    
